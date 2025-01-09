09a927a20ccccdbbbfedd91e3b6e4015980cfaa5
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-09 15:51:28 +0100

    Security Controller updated

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,33 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class LoginController extends AbstractController
 {
-    /**
-     * @Route("/connexion", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_account');
-        }
-
-        // get the login error if there is one
+        // Gérer les erreurs
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+
+        // Dernier username (email)
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('login/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
     }
 
-    /**
-     * @Route("/deconnexion", name="app_logout")
-     */
-    public function logout(): void
+    #[Route('/deconnexion', name: 'app_logout')]
+    public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // controller can be blank : it will never be called
+        throw new \Exception("Dont't forget to activate logout in security.yml");
     }
 }
